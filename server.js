9879f898a57edf7ac690da436b94c76481093993
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-07-06 00:51:30 +0300

    Add functionality: Get user profile, Update entries and Sign in

--- server.js
+++ server.js
@@ -24,35 +24,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-//Temporary data
-const database = {
-    users: [
-        {
-            id: "123",
-            name: "John",
-            email: "john@gmail.com",
-            password: "cookies",
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: "1234",
-            name: "Viku",
-            email: "viku@gmail.com",
-            password: "pizza",
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: "987",
-            hash: "",
-            email: "john@gmail.com"
-        }
-    ]
-}
-
 //ROUTES
 
 //ROOT route
@@ -62,72 +33,75 @@ app.get("/", (req, res)=> {
 
 //SIGN IN route
 app.post("/signin", (req, res)=> {
-    // Load hash from your password DB.
-    bcrypt.compare("password","$2a$10$rTzhowpfmdeZ.yicxyOsQum1Zl0A6Z21RX1qR3crMZml1Wc3OYTDS", 
-    function(err, res) {
-        // res == true
-        console.log("first guess", res);
-    });
-    bcrypt.compare("veggies", "$2a$10$rTzhowpfmdeZ.yicxyOsQum1Zl0A6Z21RX1qR3crMZml1Wc3OYTDS", 
-    function(err, res) {
-        // res = false
-        console.log("second guess", res);
-    });
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
+    db.select("email", "hash").from("login")
+    .where("email", "=", req.body.email)
+    .then (data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select("*").from("users")
+                .where("email", "=", req.body.email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json("unable to get user"))
         } else {
-            res.status(400).json("error logging in boi");
+            res.status(400).json("wrong credential")
         }
+    })
+    .catch(err => res.status(400).json("wrong credentials"))
 });
 
 //REGISTER route
 app.post("/register", (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-    db("users")
-    .returning("*")
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
-    })
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email:email
+        })
+        .into("login")
+        .returning("email")
+        .then(loginEmail => {
+            return trx("users")
+            .returning("*")
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            }).then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })   
     .catch(err => res.status(400).json("Unable to register, email already exists"))
 });
 
 //PROFILE route
 app.get("/profile/:id", (req, res) => {
-    let found = false;
     const {id} = req.params;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        } 
-    });
-    if (!found) {
-        res.status(400).json("Profile not found");
-    }
+    db.select("*").from("users").where({id})
+        .then(user => {
+            if(user.length){
+                res.json(user[0]);
+            } else {
+                res.status(400).json("Profile Not Found");
+            }
+        })
 });
 
 //IMAGE route
 app.put("/image", (req, res) => {
-    let found = false;
     const {id} = req.body;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
-    });
-    if (!found) {
-        res.status(400).json("Profile not found");
-    }
+    db("users").where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"))
 });
 
 app.listen(3001, ()=> {
